12d5d7766501cf788fa28e0556ad53c4462f877e
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-06-23 15:34:13 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'Dave'];
+let action = ['ate', 'lost', 'crushed', 'broke', 'stole'];
+let what = ['my homework', 'my phone', 'the car', 'my glasses'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector('#excuse').innerHTML = who[Math.floor(Math.random() * 5)]
+   + " " + action[Math.floor(Math.random() * 5)] + " " + what[Math.floor(Math.random() * 4)] + " " + when[Math.floor(Math.random() * 5)] + "."
 };
+
