0215e3e1cd9b1ea1e68a01ba4264f69eb51751ab
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-07-19 11:36:26 -0500

    Replaced hard-code in JS with funciton
    function generates random phrase to go in sentence

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-function randomPhrase() {
-  
+function randomPhrase(arrayName) {
+  arrayName[Math.floor(Math.random() * arrayName.length)]
 }
 
 let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'Dave'];
@@ -17,7 +17,7 @@ let when = ['before the class', 'when I was sleeping', 'while I was exercising',
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-  document.querySelector('#excuse').innerHTML = who[Math.floor(Math.random() * 5)]
-   + " " + action[Math.floor(Math.random() * 5)] + " " + what[Math.floor(Math.random() * 4)] + " " + when[Math.floor(Math.random() * 5)] + "."
+  document.querySelector('#excuse').innerHTML = randomPhrase(who)
+   + " " + randomPhrase(action) + " " + randomPhrase(what) + " " + randomPhrase(when) + "."
 };
 
